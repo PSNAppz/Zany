7707225a3c8938659dc4c3647764083af438e37a
P.S.Narayanan <thepsnarayanan@gmail.com>
2016-05-13 06:56:14 -0700

    Beta v1.1

--- js/active.js
+++ js/active.js
@@ -4,3 +4,31 @@ $(function(){
         $(this).addClass('active'); // add the class to the newly clicked link
     });
 });
+
+/**
+ * Add a new chat message
+ *
+ * @param {string} message
+ */
+function send_message(message) {
+  $.ajax({
+    url: 'send_msg.php',
+    method: 'post',
+    data: {msg: message},
+    success: function(data) {
+      $('#chatMsg').val('');
+      get_messages();
+    }
+  });
+}
+
+$('textarea').keypress(
+    function(e){
+        if (e.keyCode == 13) {
+            var msg = $(this).val();
+			$(this).val('');
+			if(msg!='')
+			$('<div class="msg_b">'+msg+'</div>').insertBefore('.msg_push');
+			$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
+        }
+    });
